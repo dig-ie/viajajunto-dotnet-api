dc988bdbc9b828d36d1dda204c9ed522a93e8749
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-04-20 16:43:28 -0300

    feat: mysql db and inMemory db alternatives

--- Program.cs
+++ Program.cs
@@ -1,25 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Viajajunto.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Obtenha o nome do banco de dados a ser usado (MySQL ou InMemory)
+var useInMemoryDatabase = builder.Configuration.GetValue<bool>("UseInMemoryDatabase");
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Condicionalmente escolhe o banco de dados com base na configuração
+if (useInMemoryDatabase)
+{
+    // Usa o banco de dados em memória
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseInMemoryDatabase("ViajajuntoInMemoryDb"));
+}
+else
+{
+    // Usa o MySQL
+    var connectionString = builder.Configuration.GetConnectionString("MySQLConnection");
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseMySQL(connectionString));
+}
+
+// Outros serviços e configurações
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Configuração de middlewares
+app.UseRouting();
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
