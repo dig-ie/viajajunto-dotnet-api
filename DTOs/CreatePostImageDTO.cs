97ed2ea971e43d7b2a329c5269f97e938fcdd349
Thiago Felipe <thiago98felipec@gmail.com>
2025-05-03 19:02:27 -0300

    feat: add create post image DTO

---
+++ DTOs/CreatePostImageDTO.cs
@@ -0,0 +1,8 @@
+namespace Viajajunto.DTOs
+{
+    public class CreatePostImageDTO
+    {
+        public int PostId { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
