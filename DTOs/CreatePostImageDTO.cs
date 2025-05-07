27e442cbbcd788bde4b238e9f807f87d23843dfb
Thiago Felipe <thiago98felipec@gmail.com>
2025-05-07 13:03:08 -0300

    style: file renamed for PostImage.cs

--- DTOs/CreatePostImageDTO.cs
+++
@@ -1,8 +0,0 @@
-namespace Viajajunto.DTOs
-{
-    public class CreatePostImageDTO
-    {
-        public int PostId { get; set; }
-        public string ImageUrl { get; set; } = string.Empty;
-    }
-}
