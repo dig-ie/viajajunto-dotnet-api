2fecaea4a98b90ed356ca195829ea26f2cb0481b
Thiago Felipe <thiago98felipec@gmail.com>
2025-05-07 13:02:47 -0300

    style: file renamed for Post.cs

--- DTOs/CreatePostDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace Viajajunto.DTOs
-{
-    public class CreatePostDTO
-    {
-        public int MarkPointId { get; set; }
-        public int UserId { get; set; }
-        public string Content { get; set; } = string.Empty;
-    }
-}
