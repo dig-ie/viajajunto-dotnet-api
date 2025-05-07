fcf092e8075b3c8f2da13e47de0cc82a023f1c16
Thiago Felipe <thiago98felipec@gmail.com>
2025-05-07 14:37:41 -0300

    refactor: changing instantiation to direct constructor to match use of "record" and comments removed

--- ViajaJunto.Tests/PostServiceTests.cs
+++ ViajaJunto.Tests/PostServiceTests.cs
@@ -13,7 +13,7 @@ namespace Viajajunto.Tests.Services
         private ApplicationDbContext GetInMemoryDbContext()
         {
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()) // Novo banco para testar
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
                 .Options;
 
             return new ApplicationDbContext(options);
@@ -22,20 +22,12 @@ namespace Viajajunto.Tests.Services
         [Fact]
         public void CreatePost_ShouldAddPostToDatabase()
         {
-            // Arrange
             var context = GetInMemoryDbContext();
             var service = new PostService(context);
-            var dto = new CreatePostDTO
-            {
-                MarkPointId = 1,
-                UserId = 42,
-                Content = "Primeiro post de teste"
-            };
+            var dto = new CreatePostDTO(1, 42, "Primeiro post de teste");
 
-            // Act
             var result = service.CreatePost(dto);
 
-            // Assert
             var postInDb = context.Posts.FirstOrDefault();
             Assert.NotNull(postInDb);
             Assert.Equal(dto.Content, postInDb.Content);
@@ -45,7 +37,6 @@ namespace Viajajunto.Tests.Services
         [Fact]
         public void GetAllPost_ShouldReturnAllPosts()
         {
-            // Arrange
             var context = GetInMemoryDbContext();
             context.Posts.Add(new Models.Post(1, 1, "Post 1"));
             context.Posts.Add(new Models.Post(2, 1, "Post 2"));
@@ -53,10 +44,8 @@ namespace Viajajunto.Tests.Services
 
             var service = new PostService(context);
 
-            // Act
             var posts = service.GetAllPosts();
 
-            // Assert
             Assert.Equal(2, posts.Count);
             Assert.Contains(posts, p => p.Content == "Post 1");
             Assert.Contains(posts, p => p.Content == "Post 2");
